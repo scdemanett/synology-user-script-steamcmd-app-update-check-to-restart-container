229c1af055b2d737a415ee9e768f1cc3cf18d891
Steven Demanett <stevendemanett@gmail.com>
2023-11-01 14:53:04 -0400

    Update Script For An Array Of AppIDs

--- steamcmd-update-check-restart-container.sh
+++ steamcmd-update-check-restart-container.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
 # Define the path to the container
-CONTAINER_PATH=""
+CONTAINER_PATH="volume1/docker"
 
-# Define the appID and container name
-APP_ID=""
-CONTAINER_NAME=""
+# Define the array of appIDs and container name
+APP_IDS=("228980" "728470") # Replace with actual APP_IDs
+CONTAINER_NAME="astroneer"
 
-# Path to the appmanifest file
-MANIFEST_PATH="/${CONTAINER_PATH}/${CONTAINER_NAME}/gamefiles/steamapps/appmanifest_${APP_ID}.acf"
+# Initialize a variable to determine if a restart is needed
+RESTART_NEEDED=0
 
-# Extract the current build ID
-CURRENT_BUILD_ID=$(grep buildid "$MANIFEST_PATH" | cut -d'"' -f4)
-if [[ $? -ne 0 ]]; then
-    echo "Error: Failed to extract current build ID."
-    exit 1
-fi
+# Loop through each APP_ID and its corresponding MANIFEST_PATH
+for APP_ID in "${APP_IDS[@]}"; do
+    MANIFEST_PATH="/${CONTAINER_PATH}/${CONTAINER_NAME}/gamefiles/steamapps/appmanifest_${APP_ID}.acf"
 
-# Get the latest build ID from the SteamCMD API
-LATEST_BUILD_ID=$(curl -s -N -X GET "https://api.steamcmd.net/v1/info/${APP_ID}" | jq ".data.\"${APP_ID}\".depots.branches.public.buildid" | xargs printf '%d')
-if [[ $? -ne 0 ]]; then
-    echo "Error: Failed to retrieve latest build ID."
-    exit 1
-fi
+    # Extract the current build ID
+    CURRENT_BUILD_ID=$(grep buildid "$MANIFEST_PATH" | cut -d'"' -f4)
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to extract current build ID for APP_ID ${APP_ID}."
+        continue
+    fi
 
-# Compare the build IDs and restart the Docker container if an update is available
-if (( CURRENT_BUILD_ID < LATEST_BUILD_ID ))
-then
-    echo "${CONTAINER_NAME} update available. Restarting the container..."
+    # Get the latest build ID from the SteamCMD API
+    LATEST_BUILD_ID=$(curl -s -N -X GET "https://api.steamcmd.net/v1/info/${APP_ID}" | jq ".data.\"${APP_ID}\".depots.branches.public.buildid" | xargs printf '%d')
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to retrieve latest build ID for APP_ID ${APP_ID}."
+        continue
+    fi
+
+    # Compare the build IDs and set the restart flag if an update is available
+    if (( CURRENT_BUILD_ID < LATEST_BUILD_ID )); then
+        echo "Update available for ${CONTAINER_NAME} for APP_ID ${APP_ID}."
+        RESTART_NEEDED=1
+    else
+        echo "No update available for ${CONTAINER_NAME} for APP_ID ${APP_ID}."
+    fi
+done
+
+# Restart the Docker container if an update is available
+if (( RESTART_NEEDED == 1 )); then
+    echo "Updates were found. Restarting the container ${CONTAINER_NAME}..."
     docker restart "${CONTAINER_NAME}"
 else
-    echo "No update available for ${CONTAINER_NAME}."
+    echo "No updates were found. No need to restart the container."
 fi
